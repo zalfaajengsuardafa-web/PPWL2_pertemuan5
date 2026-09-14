dacb0d1e97b6963114c2507447c06c7e7d8a8bce
Zalfa Ajeng Suardafa <zalfaajengsuardafa@mail.ugm.ac.id>
2026-09-14 19:23:39 +0700

    Membuat BukuController

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,40 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; // Jika pakai Query Builder
 
 class BukuController extends Controller
 {
     public function index()
     {
-        $daftarBuku = DB::table('buku')
+        $buku = DB::table('buku')
             ->orderBy('judul', 'asc')
             ->get();
 
-        return view('buku.index', compact('daftarBuku'));
+        return view('buku', compact('buku'));
     }
 
-    public function create()
-    {
-    }
-
-    public function store(Request $request)
-    {
-    }
-
-    public function show(string $id)
-    {
-    }
-
-    public function edit(string $id)
-    {
-    }
-
-    public function update(Request $request, string $id)
-    {
-    }
-
-    public function destroy(string $id)
-    {
-    }
+    public function create() {}
+    public function store(Request $request) {}
+    public function show(string $id) {}
+    public function edit(string $id) {}
+    public function update(Request $request, string $id) {}
+    public function destroy(string $id) {}
 }
