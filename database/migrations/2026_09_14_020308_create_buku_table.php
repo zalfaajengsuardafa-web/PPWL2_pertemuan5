63107712569ca46a4ecded568f3274b0eeacf00c
Zalfa Ajeng Suardafa <zalfaajengsuardafa@mail.ugm.ac.id>
2026-09-14 12:01:14 +0700

    database: add migrations and factories

---
+++ database/migrations/2026_09_14_020308_create_buku_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations
+     */
+    public function up(): void
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('penulis');
+            $table->integer('tahun_terbit');
+            $table->integer('stok')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku');
+    }
+};
