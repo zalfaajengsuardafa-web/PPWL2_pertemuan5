0792a723b7df40571f3b86c1709d401ce9abfe77
Zalfa Ajeng Suardafa <zalfaajengsuardafa@mail.ugm.ac.id>
2026-09-14 12:01:35 +0700

    view: add header and footer partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer style="background: #e2e8f0; padding: 10px; margin-top: 20px;">
+    <p>&copy; 2026 Laravel Project Ajeng</p>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header style="background: #e2e8f0; padding: 10px;">
+    <h1>Header Website Ajeng</h1>
+</header>
