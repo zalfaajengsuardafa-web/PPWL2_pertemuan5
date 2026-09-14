24bae7da1a79f81ca9a640b4835a9018dd30a802
Zalfa Ajeng Suardafa <zalfaajengsuardafa@mail.ugm.ac.id>
2026-09-14 12:01:08 +0700

    feat: add BukuController resource

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BukuController extends Controller
+{
+    public function index()
+    {
+        $daftarBuku = DB::table('buku')
+            ->orderBy('judul', 'asc')
+            ->get();
+
+        return view('buku.index', compact('daftarBuku'));
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(string $id)
+    {
+    }
+
+    public function edit(string $id)
+    {
+    }
+
+    public function update(Request $request, string $id)
+    {
+    }
+
+    public function destroy(string $id)
+    {
+    }
+}
